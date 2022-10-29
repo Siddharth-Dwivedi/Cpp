77e7b1cb21cdc3de33623aa21f95a7a02afef02d
Siddharth Dwivedi <82795203+Siddharth-Dwivedi@users.noreply.github.com>
2022-10-29 23:41:14 +0530

    Add files via upload

---
+++ Factorise_NplusM.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n==2){
+        cout<<2<<endl;
+    }
+    else{
+        cout<<3<<endl;
+    }
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
