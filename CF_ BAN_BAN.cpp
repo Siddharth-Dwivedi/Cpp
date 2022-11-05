b77b94d4da6c0ee2f490afdfb7c929b3e9d5dc40
Siddharth Dwivedi <82795203+Siddharth-Dwivedi@users.noreply.github.com>
2022-11-05 16:29:21 +0530

    Update CF_ BAN_BAN.cpp

--- CF_ BAN_BAN.cpp
+++ CF_ BAN_BAN.cpp
@@ -21,3 +21,31 @@ int main(){
 
 
 
+// Wrong Code Logic
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int t;
+//     cin>>t;
+//     while(t--){
+//         int n;
+//         cin>>n;
+//         int k = n%2+n/2;
+//         cout<<k<<endl;
+//         int m = n*3;
+//         int p =1,q=6;
+//         for(int i=1;i<=k;i++){
+//             if(m==3){
+//                 cout<<p<<" "<<p+2<<endl;
+//                 break;
+//             }
+//             else{
+//                 cout<<p<<" "<<q<<endl;
+//                 p = p+6;
+//                 q = q+6;
+//                 m= m -6;
+//             }
+//         }
+//     }
+// }
